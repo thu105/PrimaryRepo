f2af7c4b9ac0a993e842758b3e762a69fc4d9abc
Michael Fahy <fahy@chapman.edu>
2016-02-16 21:41:23 -0800

    adds example client and server

---
+++ UDPServer.java
@@ -0,0 +1,57 @@
+/**
+*	UDP Server Program
+*	Listens on a UDP port
+*	Receives a line of input from a UDP client
+*	Returns an upper case version of the line to the client
+*
+*	@author: Michael Fahy
+@	version: 2.0
+*/
+
+import java.io.*;
+import java.net.*;
+
+class UDPServer {
+  public static void main(String args[]) throws Exception
+    {
+
+    DatagramSocket serverSocket = null;
+	  
+	try
+		{
+			serverSocket = new DatagramSocket(9876);
+		}
+	
+	catch(Exception e)
+		{
+			System.out.println("Failed to open UDP socket");
+			System.exit(0);
+		}
+
+      byte[] receiveData = new byte[1024];
+      byte[] sendData  = new byte[1024];
+
+      while(true)
+        {
+
+          DatagramPacket receivePacket =
+             new DatagramPacket(receiveData, receiveData.length);
+           serverSocket.receive(receivePacket);
+          String sentence = new String(receivePacket.getData());
+
+          InetAddress IPAddress = receivePacket.getAddress();
+
+          int port = receivePacket.getPort();
+
+          String capitalizedSentence = sentence.toUpperCase();
+
+          sendData = capitalizedSentence.getBytes();
+
+          DatagramPacket sendPacket =
+             new DatagramPacket(sendData, sendData.length, IPAddress,
+                               port);
+
+          serverSocket.send(sendPacket);
+        }
+    }
+}
